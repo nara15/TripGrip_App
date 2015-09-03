6baaeaaf90bfc1b0112edecc1f34842bc31b2d84
Mario Naranjo <marionaranjo12@gmail.com>
2015-09-03 16:30:10 -0600

    Versión 1

---
+++ app/src/main/java/com/example/josemario/tripgrip_app/MyApplication.java
@@ -0,0 +1,44 @@
+package com.example.josemario.tripgrip_app;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Jose Mario on 03/09/2015.
+ */
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        printHashKey();
+    }
+
+    //Método para imprimir el Hash Key del ambiente de desarrollo
+    public void printHashKey()
+    {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.josenaranjo.tripgrip_app",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("android:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+
+}
